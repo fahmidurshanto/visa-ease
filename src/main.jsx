1851494af9090927da1230c6bfb5ad13e3aafb0c
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2025-03-04 02:47:34 +0600

    Added context api

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import 'animate.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './Routes/Router'
+import AuthProvider from './providers/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <RouterProvider router={Router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,64 @@
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../firebase/firebase.config";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth).then(() => {
+        alert("Logout successfully")
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = () => {
+      onAuthStateChanged(auth, (user) => {
+        console.log(`Current user's email is : ${user.email}`);
+        setUser(user);
+        setLoading(false);
+        return ()=>{
+            unsubscribe()
+        }
+      });
+    };
+  }, []);
+
+  const authInfo = {
+    createUser,
+    user,
+    signIn,
+    googleLogin,
+    logout,
+    loading
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
